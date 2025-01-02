1ef07b0afbe9d2e5f14761587b6bc418099253cc
Louise <emmalouisekarlsson@hotmail.com>
2025-01-02 23:29:04 +0100

    config multi pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/<Test-Kino-deploy/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        kids: resolve(__dirname, 'pages/kids/kids.html'),
+        about: resolve(__dirname, 'pages/about/about.html')
+      }
+    }
+  },
   css: {
     devSourcemap: true,
   },
