a1d499811dbd1c15434f96c846694fd89758e068
Louise <emmalouisekarlsson@hotmail.com>
2025-01-03 12:14:37 +0100

    new build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/<Test-Kino-deploy/',
   build: {
     rollupOptions: {
       input: {
