1c335e1fd27409b66420e998e294201174ea57d9
Louise <emmalouisekarlsson@hotmail.com>
2025-01-12 20:49:38 +0100

    server works

--- server.js
+++ server.js
@@ -3,24 +3,13 @@ import fs from 'fs/promises';
 
 const app = express();
 
-//Skapar en rout som ska reagera när en klient gör en GET request
+app.use("/Test-Kino-deploy/", express.static("dist"));
+app.use(express.static("dist"));
+
 app.get("/", async (req, res) => {
-  const buf = await fs.readFile("index.html");
+  const buf = await fs.readFile("./dist/index.html");
   const html = buf.toString();
   res.send(html);
 });
-
-app.use("/", express.static("./"));
-
-
-
-app.get('/:name', async (req, res) => {
-  const buf = await fs.readFile("index.html");
-  const html = buf.toString();
-  const name = req.params.name;
-  const ny = html.replace('okänd', name);
-
-  res.send(ny);
-})
 
 app.listen(3080); 
