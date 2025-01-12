2a432563314d57d7134b8244ca3348a1f153cbd9
Louise <emmalouisekarlsson@hotmail.com>
2025-01-12 20:36:04 +0100

    Server works

--- server.js
+++ server.js
@@ -3,24 +3,12 @@ import fs from 'fs/promises';
 
 const app = express();
 
-//Skapar en rout som ska reagera när en klient gör en GET request
-app.get("/", async (req, res) => {
-  const buf = await fs.readFile("index.html");
+app.use(express.static('dist'));
+
+app.get('/', async (req, res) => {
+  const buf = await fs.readFile("./dist/index.html");
   const html = buf.toString();
   res.send(html);
 });
-
-app.use("/", express.static("./"));
-
-
-
-app.get('/:name', async (req, res) => {
-  const buf = await fs.readFile("index.html");
-  const html = buf.toString();
-  const name = req.params.name;
-  const ny = html.replace('okänd', name);
-
-  res.send(ny);
-})
 
 app.listen(3080); 
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { dirname, resolve } from 'path';
 
 export default defineConfig({
-  base: '/Test-Kino-deploy/',
+  base: '/',
   build: {
     rollupOptions: {
       input: {
