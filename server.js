7865719e408661be10f6f93d90613658e271878b
Louise <emmalouisekarlsson@hotmail.com>
2025-01-13 22:35:05 +0100

    Home made template

--- server.js
+++ server.js
@@ -6,10 +6,28 @@ const app = express();
 app.use("/Test-Kino-deploy/", express.static("dist"));
 app.use(express.static("dist"));
 
-app.get("/", async (req, res) => {
-  const buf = await fs.readFile("./dist/index.html");
+async function renderPage(res, page) {
+  const buf = await fs.readFile(`./dist/${page}.html`);
   const html = buf.toString();
-  res.send(html);
+
+  const templateBuf = await fs.readFile("./templates/main.html");
+  const templateHtml = templateBuf.toString();
+
+  const outHtml = templateHtml.replace("%&BODY&%", html);
+
+  res.send(outHtml);
+}
+
+app.get("/", (req, res) => {
+  renderPage(res, "index");
+});
+
+app.get("/about", async (req, res) => {
+  renderPage(res, "about");
+});
+
+app.get("/kids", async (req, res) => {
+  renderPage(res, "kids");
 });
 
 app.listen(3080); 
